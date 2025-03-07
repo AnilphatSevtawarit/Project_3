6cf659ebc54a52bebd52ff544762c03f50280614
Anilphat Sevtawarit <6604305001302@student.sru.ac.th>
2025-03-07 15:03:48 +0700

    Update Dockerfile to use Node.js 20 and copy only image.jpg; modify index.js to add new routes and update server message

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+// Add this line
+
+app.get("/", (req, res) => {
+    res.send('ขจรยศ จาวยญ <br> สุพิศจา เจิมแก้ว');
+});
+app.get("/2", (req, res) => {
+    res.send('Hello, World!');
 });
 
+// Add this line
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
